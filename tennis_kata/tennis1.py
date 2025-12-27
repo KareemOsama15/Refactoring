2f1fc16907f2dfff8f2f5e5270bf179149d3654c
KareemOsama15 <karim.osama.saleh@gmail.com>
2025-12-27 18:17:29 +0200

    Refactor TennisGame1 to inherit from TennisGameInterface and update instance attributes naming

--- tennis_kata/tennis1.py
+++ tennis_kata/tennis1.py
@@ -1,27 +1,26 @@
-class TennisGame1:
-    def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.p1points = 0
-        self.p2points = 0
+from tennis_interface import TennisGameInterface
+
+class TennisGame1(TennisGameInterface):
+    def __init__(self, player1_name: str, player2_name: str) -> None:
+        super().__init__(player1_name, player2_name)
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.p1points += 1
+            self.player1_score += 1
         else:
-            self.p2points += 1
+            self.player2_score += 1
 
     def score(self):
         result = ""
         temp_score = 0
-        if self.p1points == self.p2points:
+        if self.player1_score == self.player2_score:
             result = {
                 0: "Love-All",
                 1: "Fifteen-All",
                 2: "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif self.p1points >= 4 or self.p2points >= 4:
-            minus_result = self.p1points - self.p2points
+            }.get(self.player1_score, "Deuce")
+        elif self.player1_score >= 4 or self.player2_score >= 4:
+            minus_result = self.player1_score - self.player2_score
             if minus_result == 1:
                 result = "Advantage player1"
             elif minus_result == -1:
@@ -33,10 +32,10 @@ class TennisGame1:
         else:
             for i in range(1, 3):
                 if i == 1:
-                    temp_score = self.p1points
+                    temp_score = self.player1_score
                 else:
                     result += "-"
-                    temp_score = self.p2points
+                    temp_score = self.player2_score
                 result += {
                     0: "Love",
                     1: "Fifteen",
