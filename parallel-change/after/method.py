53acb292ea384fe6fa096bc8d370d2f515887981
KareemOsama15 <karim.osama.saleh@gmail.com>
2026-01-10 16:14:20 +0200

    apply new is_authenticated version

--- parallel-change/after/method.py
+++ parallel-change/after/method.py
@@ -4,12 +4,10 @@ class AuthenticationService:
     def is_authenticated(self, role, id):
         return id == 12345
     """
+
     ROLES = ["admin", "user"]
 
-    def is_authenticated(self, id):
-        return id == 12345
-
-    def is_authenticated_refactored(self, role: str = "", id: int = 0) -> bool:
+    def is_authenticated(self, role: str = "", id: int = 0) -> bool:
         if role not in self.ROLES:
             raise ValueError("Invalid role")
         if not isinstance(id, int) or id <= 0:
@@ -22,19 +20,14 @@ class AuthenticationClient:
         self.authenticationService = authenticationService
 
     def run(self):
-        authenticated = self.authenticationService.is_authenticated(33)
-        authenticated_refactored = (
-            self.authenticationService.is_authenticated_refactored("admin", 12345)
-        )
+        authenticated = self.authenticationService.is_authenticated("admin", 12345)
         print("is authenticated: ", str(authenticated))
-        print("is authenticated refactored: ", str(authenticated_refactored))
-
 
 
 class YetAnotherClient:
     def run(self):
-        AuthenticationService().is_authenticated(100)
-        AuthenticationService().is_authenticated_refactored("admin", 12345)
+        AuthenticationService().is_authenticated("user", 33)
+
 
 if __name__ == "__main__":
     client = AuthenticationClient(AuthenticationService())
