482486c6a6b5874d238cc08acee288695cca109c
KareemOsama15 <karim.osama.saleh@gmail.com>
2026-01-10 20:11:25 +0200

    add test cases

--- GildedRose-kata/after/tests/test_gilded_rose.py
+++ GildedRose-kata/after/tests/test_gilded_rose.py
@@ -4,16 +4,75 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
+class ItemTest(unittest.TestCase):
+
+    def test_item_quality(self):
+        item = Item("Aged Brie", 2, 0)
+        self.assertEqual(0, item.quality)
+
+    def test_item_sell_in(self):
+        item = Item("Aged Brie", 2, 0)
+        self.assertEqual(2, item.sell_in)
+
+    def test_item_name(self):
+        item = Item("Aged Brie", 2, 0)
+        self.assertEqual("Aged Brie", item.name)
+
+    def test_item_quality_is_negative(self):
+        item = Item("Aged Brie", 2, -1)
+        with self.assertRaises(ValueError, msg="Quality cannot be negative"):
+            item.quality = -1
+
+
 class GildedRoseTest(unittest.TestCase):
 
-    def test_brie_quality(self):
-        """
-        Test the item name.
-        """
+    def test_aged_brie_quality(self):
         items = [Item("Aged Brie", 2, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(1, items[0].quality)
+
+    def test_sulfuras_quality(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(80, items[0].quality)
+
+    def test_backstage_passes_quality_increased_by_1(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(21, items[0].quality)
+
+    def test_backstage_passes_quality_increased_by_2(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(22, items[0].quality)
+
+    def test_backstage_passes_quality_increased_by_3(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(23, items[0].quality)
+
+    def test_backstage_passes_quality_when_sell_in_is_0(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+    def test_backstage_passes_quality_when_sell_in_is_negative(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+    def test_normal_item_quality_decreased_by_1(self):
+        items = [Item("Elixir of the Mongoose", 2, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(9, items[0].quality)
 
 
 if __name__ == "__main__":
