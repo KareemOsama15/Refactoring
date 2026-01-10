4ba000ad782ebd11372a6b1959de88af8c408df8
KareemOsama15 <karim.osama.saleh@gmail.com>
2026-01-10 15:29:44 +0200

    implement prices list beside price attr

--- parallel-change/after/field.py
+++ parallel-change/after/field.py
@@ -1,10 +1,10 @@
 class ShoppingCart:
-
-    '''
+    """
     the goal is to remove the field above, using a list of prices instead:
     def __init__(self):
         self.prices = []
-    '''
+    """
+
     def __init__(self) -> None:
         self.price = 0
         self.prices = []
@@ -18,20 +18,22 @@ class ShoppingCart:
         self.prices.append(price)
 
     def calculate_total_price(self):
-        if isinstance(self.price, list):
+        if self.prices:
             return sum(self.prices)
         return self.price
 
     def has_discount(self):
-        if isinstance(self.price, list):
+        if self.prices:
             return sum(self.prices) >= 100
         return self.price >= 100
 
     def number_of_products(self):
+        if self.prices:
+            return len(self.prices)
         return 1
 
 
-class SomeConsumer():
+class SomeConsumer:
     def do_stuff():
         shoppingCart = ShoppingCart()
         shoppingCart.add(100)
@@ -42,6 +44,7 @@ if __name__ == "__main__":
     shoppingCart = ShoppingCart()
     # shoppingCart.add(10)
     shoppingCart.add_item(20)
+    shoppingCart.add_item(100)
     print("number of products:", shoppingCart.number_of_products())
     print("total price:", shoppingCart.calculate_total_price())
     print("has discount:", shoppingCart.has_discount())
