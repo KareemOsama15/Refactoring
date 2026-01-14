c4f69b01e405d785b768bd340a225850a8c5a0e1
KareemOsama15 <karim.osama.saleh@gmail.com>
2026-01-14 12:00:54 +0200

    implement BackstagePassesUpdaterto update item fields

--- GildedRose-kata/after/gilded_rose.py
+++ GildedRose-kata/after/gilded_rose.py
@@ -28,6 +28,7 @@ class Item:
             raise ValueError("Quality cannot be greater than 50")
         self._quality = value
 
+
 class ItemUpdaterInterface(ABC):
     """Item updater interface."""
 
@@ -52,12 +53,38 @@ class AgedBrieUpdater(ItemUpdaterInterface):
         """Update the quality of the item."""
         self._update_sell_in(item)
         self._update_quality(item)
-    
+
     def _update_quality(self, item: Item) -> None:
         """Update the quality of the item."""
         if item.quality < 50:
             item.quality += 1
-    
+
+    def _update_sell_in(self, item: Item) -> None:
+        """Update the sell_in of the item."""
+        item.sell_in -= 1
+
+
+class BackstagePassesUpdater(ItemUpdaterInterface):
+    """Backstage passes updater class."""
+
+    def update(self, item: Item) -> None:
+        self._update_sell_in(item)
+        self._update_quality(item)
+
+    def _update_quality(self, item: Item) -> None:
+        """Update the quality of the item."""
+        # Quality Increase when: quality can't be greater than 50 and sell_in is greater than 0
+        if item.quality < 50 and item.sell_in > 0:
+            if item.sell_in <= 5:
+                item.quality += 3
+            elif item.sell_in <= 10:
+                item.quality += 2
+            else:
+                item.quality += 1
+        # sell_in is less than or equal to 0, quality is set to 0
+        elif item.sell_in <= 0:
+            item.quality = 0
+
     def _update_sell_in(self, item: Item) -> None:
         """Update the sell_in of the item."""
         item.sell_in -= 1
@@ -72,6 +99,7 @@ class GildedRose(object):
         self.updaters: Dict[str, ItemUpdaterInterface] = {
             "Sulfuras, Hand of Ragnaros": SulfurasUpdater(),
             "Aged Brie": AgedBrieUpdater(),
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassesUpdater(),
         }
 
     def update_quality(self) -> None:
