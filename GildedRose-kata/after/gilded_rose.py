56723b2b07860e01033f195e1bc50799cbb6a614
KareemOsama15 <karim.osama.saleh@gmail.com>
2026-01-14 10:35:16 +0200

    impelment SulfurasUpdater class to update item attrs

--- GildedRose-kata/after/gilded_rose.py
+++ GildedRose-kata/after/gilded_rose.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from typing import List
+from typing import List, Dict
+from abc import ABC, abstractmethod
 
 
 class Item:
@@ -27,6 +28,20 @@ class Item:
             raise ValueError("Quality cannot be greater than 50")
         self._quality = value
 
+class ItemUpdaterInterface(ABC):
+    """Item updater interface."""
+
+    @abstractmethod
+    def update(self, item: Item) -> None:
+        """Update the quality or sell_in of the item."""
+        raise NotImplementedError("Subclasses must implement this method")
+
+class SulfurasUpdater(ItemUpdaterInterface):
+    """Sulfuras, Hand of Ragnaros updater class."""
+
+    def update(self, item: Item) -> None:
+        """Update the quality of the item."""
+        pass
 
 class GildedRose(object):
     """Gilded Rose class."""
@@ -34,6 +49,9 @@ class GildedRose(object):
     def __init__(self, items: List[Item]) -> None:
         """Initialize the Gilded Rose class."""
         self.items: List[Item] = items
+        self.updaters: Dict[str, ItemUpdaterInterface] = {
+            "Sulfuras, Hand of Ragnaros": SulfurasUpdater(),
+        }
 
     def update_quality(self) -> None:
         """
@@ -41,13 +59,17 @@ class GildedRose(object):
         """
 
         for item in self.items:
+            updater = self.updaters.get(item.name)
+            if updater:
+                updater.update(item)
+                continue
+
             if (
                 item.name != "Aged Brie"
                 and item.name != "Backstage passes to a TAFKAL80ETC concert"
             ):
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -58,14 +80,12 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+            item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                            item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
